56e6d07449f7d67a4b1e7aafb1d3e0d6882289e9
Ehsan Amid <amid.ehsan@gmail.com>
2022-06-24 16:38:38 +0800

    20220624

--- PercolationStats.java
+++ PercolationStats.java
@@ -1,38 +1,49 @@
+
+import edu.princeton.cs.algs4.StdOut;
+
 public class PercolationStats {
 
-    
     // perform independent trials on an n-by-n grid
-    public PercolationStats(int n, int trials)
-    {
+    public PercolationStats(int n, int trials) {
 
     }
 
     // sample mean of percolation threshold
-    public double mean()
-    {
+    public double mean() {
         return 0.0;
     }
 
     // sample standard deviation of percolation threshold
-    public double stddev()
-    {
+    public double stddev() {
         return 0.0;
     }
 
     // low endpoint of 95% confidence interval
-    public double confidenceLo()
-    {
+    public double confidenceLo() {
         return 0.0;
     }
+
     // high endpoint of 95% confidence interval
-    public double confidenceHi()
-    {
+    public double confidenceHi() {
         return 0.0;
     }
-   // test client (see below)
-   public static void main(String[] args)
-   {
-       
-   }
+
+    // test client (see below)
+    public static void main(String[] args) {
+
+        int n = 0;
+        int T = 0;
+        if (args.length == 2) {
+            n = Integer.parseInt(args[0]);
+            T = Integer.parseInt(args[1]);
+        } else {
+            n = 10;
+            T = 100;
+        }
+        PercolationStats ps = new PercolationStats(n, T);
+        StdOut.printf("mean                    = %f\n", ps.mean());
+        StdOut.printf("stddev                  = %f\n", ps.stddev());
+        StdOut.printf("95%% confidence interval = %f, %f\n", ps.confidenceLo(), ps.confidenceHi());
+    }
 
 }
--- TestPercolation.java
+++ TestPercolation.java
@@ -1,15 +1,15 @@
-import edu.princeton.cs.algs4.StdOut;
 
 public class TestPercolation {
 
-    public  static void main(String[] args) 
-    {
+    public static void main(String[] args) {
         Percolation perc = new Percolation(5);
 
-        perc.open(3, 1);
-        perc.open(3, 2);
-        perc.open(2, 2);
-        boolean ret =  perc.percolates();
-        StdOut.print(ret);
+        // perc.open(1, 1);
+        // perc.open(2, 1);
+        // perc.open(3, 1);
+        // perc.open(4, 1);
+        // perc.open(5, 1);
+        perc.percolates();
+
     }
 }
